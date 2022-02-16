f4de8088e06580d5613c63f8c805846990ed131a
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-16 09:46:20 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'@PolyZone/CircleZone.lua',
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+shared_scripts { "shared/*.lua" }
+
