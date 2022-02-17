89a8f9dd7fba08f27cd2ad26c0aa465eff46aad0
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-17 10:06:38 +0100

    Multi Langagues

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+local Translations = {
+    error = {
+     	enter_gangzone = "You have entered a gang territory !",
+    	leave_gangzone = "You have left a gang territory !",
+    	hostile_zone = "Hostile Zone",
+    }
+}
