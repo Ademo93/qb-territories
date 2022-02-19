857400edaa394689810563c02962611673d67e67
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-19 03:49:18 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -3,6 +3,7 @@ local Translations = {
      	enter_gangzone = "You have entered a gang territory !",
     	leave_gangzone = "You have left a gang territory !",
     	hostile_zone = "Hostile Zone",
+        member_not_connected = "Not enough enemy gang member connected !",
     }
 }
 Lang = Locale:new({
