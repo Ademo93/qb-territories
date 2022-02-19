84b7ad41fe2ff8f70ad47dbdf8c15db119276082
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-19 03:49:00 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -3,6 +3,7 @@ local Translations = {
         enter_gangzone = "Vous êtes entré sur le territoire d'un gang !",
         leave_gangzone = "Vous avez quitté le territoire d'un gang !",
         hostile_zone = "Zone Hostile",
+        member_not_connected = "Pas assez de membres ennemies connecter !",
     }
 }
 Lang = Locale:new({
