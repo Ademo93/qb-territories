d3dc74551308b836c22473d165f2156dde388d72
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-19 03:47:36 +0100

    zone control

--- client/main.lua
+++ client/main.lua
@@ -42,16 +42,16 @@ CreateThread(function()
 
         local blip = AddBlipForRadius(v.centre.x, v.centre.y, v.centre.z, v.radius)
         SetBlipAlpha(blip, 80) -- Change opacity here
-        SetBlipColour(blip, Zones["Colours"][v.winner] ~= nil and Zones["Colours"][v.winner] or Zones["Colours"].neutral)
+        SetBlipColour(blip, Zones["Gangs"][v.winner].color ~= nil and Zones["Gangs"][v.winner].color or Zones["Gangs"]["neutral"].color)
 
 
         local blip2 = AddBlipForCoord(v.centre.x, v.centre.y, v.centre.z)
         SetBlipSprite (blip2, v.blip)
         SetBlipDisplay(blip2, 4)
         SetBlipAsShortRange(blip2, true)
-        SetBlipColour(blip2, Zones["Colours"][v.winner] ~= nil and Zones["Colours"][v.winner] or Zones["Colours"].neutral)
+        SetBlipColour(blip2, Zones["Gangs"][v.winner].color ~= nil and Zones["Gangs"][v.winner].color or Zones["Gangs"]["neutral"].color)
         BeginTextCommandSetBlipName("STRING")
-        AddTextComponentSubstringPlayerName(v.winner)
+        AddTextComponentSubstringPlayerName(Zones["Gangs"][v.winner].name)
         EndTextCommandSetBlipName(blip2)
 
         Territories[k] = {
@@ -62,48 +62,85 @@ CreateThread(function()
     end
 end)
 
+RegisterNetEvent("qb-gangs:client:updateblips")
+AddEventHandler("qb-gangs:client:updateblips", function(zone, winner)
+    local colour = Zones["Colours"][winner]
+   -- local blip = AddBlipForRadius(Zones["Territories"][zone].centre.x, Zones["Territories"][zone].centre.y, Zones["Territories"][zone].centre.z, Zones["Territories"][zone].radius)
+    local blip = Territories[zone].blip
+    SetBlipColour(blip,colour)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(winner)
+    EndTextCommandSetBlipName(blip)
+
+
+ 
+end)
+
+function isContested(tab)
+    local count = 0
+    for k, v in pairs(tab) do
+        count = count + 1
+    end
+
+    if count > 1 then
+        return "contested"
+    end
+    return ""
+end
+
 CreateThread(function()
     while true do 
         Wait(500)
         if isLoggedIn then
             
-                local PlayerPed = PlayerPedId()
-                local pedCoords = GetEntityCoords(PlayerPed)
-
-                for k, zone in pairs(Territories) do
-                    if Territories[k].zone:isPointInside(pedCoords) then
-                        insidePoint = true
-                        activeZone = Territories[k].id
-                        
-                        TriggerEvent("QBCore:Notify",Lang:t("error.enter_gangzone"), "error")
-
-                       
-                        while insidePoint == true do
-                            
-                           
-                                exports['qb-drawtext']:DrawText(Lang:t("error.hostile_zone"),'right')
-                            
-                           
-                            
-
-                            if not Territories[k].zone:isPointInside(GetEntityCoords(PlayerPed)) then
-
-                                insidePoint = false
-                                activeZone = nil
-
-                                QBCore.Functions.Notify(Lang:t("error.leave_gangzone"), "error")
-
-                                Wait(1000)
-
-                            end
-
-                            Wait(1000)
-                        end
-                        exports['qb-drawtext']:HideText()
+            local PlayerPed = PlayerPedId()
+            local pedCoords = GetEntityCoords(PlayerPed)
+                    
+            for k, v in pairs(Zones["Territories"]) do
+    
+                while isContested(v.occupants) == "contested" do
+                    for i,v in pairs((v.occupants)) do
+                        last = #(v.occupants) - 0
                     end
+
+                    --local blip = AddBlipForRadius(v.centre.x, v.centre.y, v.centre.z, v.radius)
+                    --SetBlipAlpha(blip, 80) -- Change opacity here
+                    --SetBlipColour(blip, Zones["Gangs"][v.winner].color ~= nil and Zones["Gangs"][v.winner].color or Zones["Gangs"]["neutral"].color)
+                    --Wait(100)
+                    --SetBlipAlpha(blip, 80) -- Change opacity here
+                    --SetBlipColour(blip,Zones["Gangs"][v.last.label].color)
+                    
                 end
-                Wait(2000)
-         
+            end 
+
+            for k, zone in pairs(Territories) do  
+
+                if Territories[k].zone:isPointInside(pedCoords) then
+                    insidePoint = true
+                    activeZone = Territories[k].id
+                        
+                    TriggerEvent("QBCore:Notify",Lang:t("error.enter_gangzone"), "error")
+          
+                    while insidePoint == true do   
+                        exports['qb-drawtext']:DrawText(Lang:t("error.hostile_zone"),'right')
+                        if PlayerGang.name ~= "none" then
+                            TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, true) 
+                        end   
+                        if not Territories[k].zone:isPointInside(GetEntityCoords(PlayerPed)) then
+
+                            if PlayerGang.name ~= "none" then
+                                TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, false)
+                            end
+                            insidePoint = false
+                            activeZone = nil
+                            QBCore.Functions.Notify(Lang:t("error.leave_gangzone"), "error")
+                        end
+                        Wait(1000)
+                    end
+                    exports['qb-drawtext']:HideText()
+                end
+            end  
+            Wait(2000)
         end
     end
 end)
