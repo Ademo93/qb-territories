62c39ca100e2c9021ebdf241b00dcad4a0397124
Ademo93 <66568345+Ademo93@users.noreply.github.com>
2022-02-19 03:48:31 +0100

    Update zones.lua

--- shared/zones.lua
+++ shared/zones.lua
@@ -1,7 +1,7 @@
 Zones = {
     ["Config"] = {
         debug = false,
-        minScore = 60
+        minScore = 10
     },
 
     ["Territories"] = {
@@ -9,41 +9,75 @@ Zones = {
         [1] = {
             centre = vector3(755.02, -298.71, 59.89),
             radius = 90.0,
-            winner = "Tarteret",
+            winner = "tarteret",
+            occupants={
+                ["tarteret"] = {
+                    label = "tarteret",
+                    score = 60,
+                },
+            },
             blip = 437
         },
 
         [2] = {
             centre = vector3(1373.51, -1534.22, 56.22),
             radius = 90.0,
-            winner = "Marabunta",
+            winner = "marabunta",
+            occupants={},
             blip = 437
         },
 
         [3] = {
             centre = vector3(1281.96, -1733.7, 52.53),
             radius = 90.0,
-            winner = "Aztecas",
+            winner = "aztecas",
+            occupants={},
             blip = 437
         },
 
         [4] = {
             centre = vector3(37.38, -1880.71, 22.34),
             radius = 120.0,
-            winner = "Ballas",
+            winner = "ballas",
+            occupants={},
             blip = 437
         }
     },
 
     ["Colours"] = {
-        ["Police"] = 38,
-        ["Tarteret"] = 4,
-        ["Aztecas"] = 32,
-        ["Ballas"] = 27,
-        ["Vagos"] = 5,
-        ["Marabunta"] = 29,
-        ["Mafia"] = 44,
-        ["The Lost MC"] = 40,
-        ["neutral"] = 0
+        ["tarteret"] = 4,
+        ["vagos"] = 5,
+    },
+
+    ["Gangs"] = {
+        ["tarteret"] = {
+            color = 4,
+            name = "Tarteret",
+        },
+        ["aztecas"] =  {
+            color = 32,
+            name = "Aztecas",
+        },
+        ["ballas"] = {
+            color = 27,
+            name = "Ballas",
+        },
+        ["vagos"] = {
+            color = 5,
+            name = "Vagos",
+        },
+        ["marabunta"] = {
+            color = 29,
+            name = "Marabunta",
+        },
+        ["mafia"] = {
+            color = 44,
+            name = "Mafia",
+        },
+        ["neutral"] = {
+            color = 0,
+            name = "",
+        },
     }
+
 }
